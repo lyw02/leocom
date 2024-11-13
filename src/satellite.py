69a7bb433a9b0d0064faef09a6b2a3fbad36aeff
Akshay Arora <31076363+iamakshayarora@users.noreply.github.com>
2024-11-13 16:58:56 +0000

    Update satellite.py

--- src/satellite.py
+++ src/satellite.py
@@ -1,11 +1,14 @@
 import signal
 import threading
+import socket
 from utils.simulation.satellite_emulator import SatelliteEmulator
 from utils.simulation.satellite_network import SatelliteNetwork
 
 
 satellite1 = SatelliteEmulator("127.0.0.1", "5000")
 satellite2 = SatelliteEmulator("127.0.0.1", "5001")
+ground_host = "127.0.0.1"
+ground_port = 5111
 
 satellite_network = SatelliteNetwork([satellite1, satellite2])
 
@@ -17,7 +20,10 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 for satellite in satellite_network.satellites:
-    threading.Thread(target=satellite.listen_for_data, daemon=True).start()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as gs:
+        gs.connect((ground_host, int(ground_port)))
+        print(f"[Satellite] Connected to Ground Station at {ground_host}:{ground_port}")
+        threading.Thread(target=satellite.listen_for_data, args=(gs), daemon=True).start()
 
 # Start the data routing and handover simulation
 satellite_network.start_data_routing()
